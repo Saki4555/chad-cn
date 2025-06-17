4312094c18a37914dc6e1d40accc2240c63ec73b
saki4555 <saki999v@gmail.com>
2025-06-17 16:13:17 +0600

    Initial Commit

---
+++ dashboard-1/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+
+'use client'
+import { LogOut, Moon, Settings, User, Sun } from "lucide-react";
+import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button"
+import { useTheme } from "next-themes";
+
+
+const Navbar = () => {
+      const { setTheme } = useTheme()
+
+  return (
+    <div className="p-4 flex items-center justify-between">
+      Collapse Button
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+          <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+       
+        <DropdownMenu >
+          <DropdownMenuTrigger> <Avatar>
+          <AvatarImage src="https://avatars.githubusercontent.com/u/121676920?v=4" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar></DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            
+            <DropdownMenuItem><User className="w-[1.2rem] h-[1.2rem] mr-2"/> User</DropdownMenuItem>
+            <DropdownMenuItem><Settings className="w-[1.2rem] h-[1.2rem] mr-2"/> Settings</DropdownMenuItem>
+            <DropdownMenuItem variant="destructive"><LogOut className="w-[1.2rem] h-[1.2rem] mr-2"/> User</DropdownMenuItem>
+           
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
